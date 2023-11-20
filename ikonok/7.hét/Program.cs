34f5eb688fa1f369f846d7c037f809a56150e0d6
Tóth Bence <Toth.Bence@AD.LOCAL>
2023-11-20 14:22:36 +0100

    assd

---
+++ ikonok/7.hét/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace megallok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sorok = new List<string>();
+            foreach (string item in File.ReadLines("megallok.txt", Encoding.UTF8))
+            {
+                sorok.Add(item);
+            }
+            string output = "";
+            foreach (string item in sorok)
+            {
+                output += $"\"{item}\",";
+            }
+            output = output.Trim(',');
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+        }
+    }
+}
